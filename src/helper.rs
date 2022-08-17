f1950dbac59bba19334cf7f258e3cf66245da4b4
power-unsealed <96887858+power-unsealed@users.noreply.github.com>
2022-08-17 20:25:26 +0200

    Implemented basic `unwrap_<>` fn

--- src/helper.rs
+++ src/helper.rs
@@ -1,34 +1,216 @@
-pub fn to_snake_case(input: &str) -> String {
-    // We probably wont need much more than two underscores...
-    let mut output = String::with_capacity(input.len() + 2);
+use proc_macro2::{Span, TokenStream};
+use quote::{format_ident, quote};
+use syn::{
+    spanned::Spanned, Attribute, Data, DataEnum, DeriveInput, Error, Fields, Generics, Ident,
+    Variant, Visibility,
+};
 
-    // Turn the input into snake case char by char
-    let mut last_uppercase = true;
-    let mut last_underscore = false;
-    for char in input.chars() {
-        if char.is_uppercase() {
-            // If there is an uppercase char that is not preceeded by another
-            // uppercase char or an underscore, add an underscore in front of it
-            if !last_uppercase && !last_underscore {
-                output.push('_');
+pub struct EnumInfo {
+    pub span: Span,
+    pub attributes: Vec<Attribute>,
+    pub generics: Generics,
+    pub identifier: Ident,
+    pub visibility: Visibility,
+    pub data: DataEnum,
+}
+
+pub trait ExtractEnumInfo {
+    fn extract_info(self) -> Result<EnumInfo, Error>;
+}
+
+impl ExtractEnumInfo for DeriveInput {
+    fn extract_info(self) -> Result<EnumInfo, Error> {
+        let span = self.span();
+
+        let data = match self.data {
+            Data::Enum(data) => data,
+            _ => {
+                return Err(Error::new(
+                    Span::call_site(),
+                    "Can only be derived for enums",
+                ));
             }
+        };
 
-            // Add the lower case version of it to the output
-            for char in char.to_lowercase() {
-                output.push(char);
-            }
+        Ok(EnumInfo {
+            span,
+            generics: self.generics,
+            attributes: self.attrs,
+            identifier: self.ident,
+            visibility: self.vis,
+            data,
+        })
+    }
+}
 
-            last_uppercase = true;
-        } else {
-            output.push(char);
+pub struct VariantInfo {
+    pub identifier: Ident,
+    pub snake_case: String,
+    pub data_type: TokenStream,
+    pub type_def: Option<TokenStream>,
+    pub pattern: TokenStream,
+    pub construction: TokenStream,
+}
 
-            last_uppercase = false;
+impl VariantInfo {
+    pub fn new(
+        data_type: TokenStream,
+        type_def: Option<TokenStream>,
+        pattern: TokenStream,
+        construction: TokenStream,
+        identifier: Ident,
+    ) -> VariantInfo {
+        VariantInfo {
+            snake_case: identifier.to_string().to_snake_case(),
+            identifier,
+            data_type,
+            type_def,
+            pattern,
+            construction,
         }
-
-        last_underscore = char == '_';
     }
 
-    output
+    pub fn build_unwrap(&self, parent: &EnumInfo) -> TokenStream {
+        let snake_case = &self.snake_case;
+        let data_type = &self.data_type;
+        let pattern = &self.pattern;
+        let construction = &self.construction;
+
+        let fn_ident = format_ident!("unwrap_{snake_case}");
+        let panic_msg = format!(
+            "Failed unwrapping to {}::{}. Unexpected variant",
+            parent.identifier, self.identifier,
+        );
+
+        quote! {
+            fn #fn_ident(self) -> #data_type {
+                match self {
+                    #pattern => #construction,
+                    _ => panic!(#panic_msg),
+                }
+            }
+        }
+    }
+}
+
+pub trait ExtractVariantInfo {
+    fn extract_info(self, parent: &EnumInfo) -> VariantInfo;
+}
+
+impl ExtractVariantInfo for Variant {
+    fn extract_info(self, parent: &EnumInfo) -> VariantInfo {
+        let enum_ident = &parent.identifier;
+        let identifier = self.ident;
+
+        match self.fields {
+            // Variant without data
+            Fields::Unit => VariantInfo::new(
+                quote! { () },
+                None,
+                quote! { #enum_ident::#identifier },
+                quote! { () },
+                identifier,
+            ),
+
+            // Variant with exactly one unnamed field
+            Fields::Unnamed(tuple) if tuple.unnamed.len() == 1 => {
+                let single = &tuple.unnamed[0].ty;
+                VariantInfo::new(
+                    quote! { #single },
+                    None,
+                    quote! { #enum_ident::#identifier( f0 ) },
+                    quote! { f0 },
+                    identifier,
+                )
+            }
+
+            // Variant with more than one unnamed field
+            Fields::Unnamed(tuple) => {
+                let fields: Vec<_> = tuple
+                    .unnamed
+                    .iter()
+                    .enumerate()
+                    .map(|(i, _)| format_ident!("f{i}"))
+                    .collect();
+
+                VariantInfo::new(
+                    quote! { #tuple },
+                    None,
+                    quote! { #enum_ident::#identifier( #(#fields),* ) },
+                    quote! { ( #(#fields),* ) },
+                    identifier,
+                )
+            }
+
+            // Variant with named fields
+            Fields::Named(fields) => {
+                let enum_attrs = &parent.attributes;
+                let enum_vis = &parent.visibility;
+                let type_ident = format_ident!("{enum_ident}{identifier}");
+
+                let (field_idents, field_types): (Vec<_>, Vec<_>) = fields
+                    .named
+                    .into_iter()
+                    .map(|field| (field.ident.unwrap(), field.ty))
+                    .unzip();
+
+                let fields: Vec<_> = field_idents
+                    .iter()
+                    .zip(field_types.iter())
+                    .map(|(i, t)| quote! { pub #i: #t })
+                    .collect();
+
+                VariantInfo::new(
+                    quote! { #type_ident },
+                    Some(quote! {
+                        #enum_vis struct #type_ident { #(#fields),* }
+                    }),
+                    quote! { #enum_ident::#identifier { #(#field_idents),* } },
+                    quote! { #type_ident { #(#field_idents),* } },
+                    identifier,
+                )
+            }
+        }
+    }
+}
+
+pub trait SnakeCase {
+    fn to_snake_case(self) -> String;
+}
+
+impl<T: AsRef<str>> SnakeCase for T {
+    fn to_snake_case(self) -> String {
+        // We probably wont need much more than two underscores...
+        let mut output = String::with_capacity(self.as_ref().len() + 2);
+
+        // Turn the input into snake case char by char
+        let mut last_uppercase = true;
+        let mut last_underscore = false;
+        for char in self.as_ref().chars() {
+            if char.is_uppercase() {
+                // If there is an uppercase char that is not preceeded by another
+                // uppercase char or an underscore, add an underscore in front of it
+                if !last_uppercase && !last_underscore {
+                    output.push('_');
+                }
+
+                // Add the lower case version of it to the output
+                for char in char.to_lowercase() {
+                    output.push(char);
+                }
+
+                last_uppercase = true;
+            } else {
+                output.push(char);
+
+                last_uppercase = false;
+            }
+
+            last_underscore = char == '_';
+        }
+
+        output
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -37,11 +219,13 @@ pub fn to_snake_case(input: &str) -> String {
 
 #[cfg(test)]
 mod tests {
+    use crate::helper::SnakeCase;
+
     #[test]
     fn to_snake_case() {
-        assert_eq!(super::to_snake_case("IpAddress"), "ip_address");
-        assert_eq!(super::to_snake_case("TCP"), "tcp");
-        assert_eq!(super::to_snake_case("snake_case"), "snake_case");
-        assert_eq!(super::to_snake_case("HOME_IP"), "home_ip");
+        assert_eq!("IpAddress".to_snake_case(), "ip_address");
+        assert_eq!("TCP".to_snake_case(), "tcp");
+        assert_eq!("snake_case".to_snake_case(), "snake_case");
+        assert_eq!("HOME_IP".to_snake_case(), "home_ip");
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-use proc_macro2::{TokenStream, Span};
-use quote::quote;
-use syn::{parse_macro_input, Ident, DeriveInput, Data, Fields, spanned::Spanned, Error};
+use helper::{ExtractEnumInfo, ExtractVariantInfo};
+use proc_macro2::TokenStream;
+use quote::{quote, ToTokens};
+use syn::{parse_macro_input, DeriveInput};
 
 mod helper;
 
@@ -11,122 +12,46 @@ pub fn unwrap_as_entry(input: proc_macro::TokenStream) -> proc_macro::TokenStrea
 }
 
 fn unwrap_as(input: DeriveInput) -> TokenStream {
-    let span = input.span();
-    let visibility = input.vis;
-    let ident = input.ident;
-    let name = ident.to_string();
-    let data = match input.data {
-        Data::Enum(data) => data,
-        _ => {
-            return Error::new(span, "`UnwrapAs` can only be derived for enums")
-                .to_compile_error();
-        }
+    let parent = match input.extract_info() {
+        Ok(enum_info) => enum_info,
+        Err(error) => return error.to_compile_error(),
     };
 
+    let variants: Vec<_> = parent
+        .data
+        .variants
+        .iter()
+        .map(|var| var.clone().extract_info(&parent))
+        .collect();
+
     let mut functions = Vec::new();
-    let mut types = Vec::new();
-    for variant in data.variants {
-        let var_ident = variant.ident;
-        let var_name = var_ident.to_string();
+    let mut type_defs = Vec::new();
 
-        // Build function identifier with prefix and variant name in snake_case
-        let sc_ident = helper::to_snake_case(&var_name);
-        let unwrap_as_name = format!("unwrap_as_{sc_ident}");
-        let unwrap_as_ident = Ident::new(&unwrap_as_name, Span::call_site());
+    for variant in variants {
+        functions.push(variant.build_unwrap(&parent));
 
-        // Build return type definition, match pattern, and instance construction code
-        let (type_ident, type_def, pattern, construction) = match variant.fields {
-            // Variant without data
-            Fields::Unit => (
-                quote! { () },
-                quote! { },
-                quote! { #ident::#var_ident },
-                quote! { () },
-            ),
-            // Variant with exactly one unnamed field
-            Fields::Unnamed(tuple) if tuple.unnamed.len() == 1 => {
-                let single = &tuple.unnamed[0].ty;
-                (
-                    quote! { #single },
-                    quote! { },
-                    quote! { #ident::#var_ident( f0 ) },
-                    quote! { f0 },
-                )
-            },
-            // Variant with more than one unnamed field
-            Fields::Unnamed(tuple) => {
-                let fields: Vec<_> = tuple.unnamed.iter().enumerate()
-                    .map(|(i, _)| {
-                        let name = format!("f{i}");
-                        Ident::new(&name, Span::call_site())
-                    })
-                    .collect();
-
-                (
-                    quote! { #tuple },
-                    quote! { },
-                    quote! { #ident::#var_ident( #(#fields),* ) },
-                    quote! { ( #(#fields),* ) },
-                )
-            },
-            // Variant with named fields
-            Fields::Named(fields) => {
-                // Build return type identifier with prefix, enum name, and variant name
-                // in CamelCase
-                let type_name = format!("UnwrapAs{name}{var_name}");
-                let type_ident = Ident::new(&type_name, Span::call_site());
-
-                let (field_idents, field_types): (Vec<_>, Vec<_>) = fields.named.into_iter()
-                    .map(|field| {
-                        (field.ident.unwrap(), field.ty)
-                    })
-                    .unzip();
-                let fields: Vec<_> = field_idents.iter().zip(field_types.iter())
-                    .map(|(i, t)| quote! { pub #i: #t })
-                    .collect();
-                
-                (
-                    quote! { #type_ident },
-                    quote! { #visibility struct #type_ident { #(#fields),* } },
-                    quote! { #ident::#var_ident { #(#field_idents),* } },
-                    quote! { #type_ident { #(#field_idents),* } },
-                )
-            },
-        };
-
-        let panic_msg = format!(
-            "Failed unwrapping to {name}::{var_name}. Unexpected variant"
-        );
-
-        let function = quote! {
-            fn #unwrap_as_ident(self) -> #type_ident {
-                match self {
-                    #pattern => #construction,
-                    _ => panic!(#panic_msg),
-                }
-            }
-        };
-
-        functions.push(function);
-        if !type_def.is_empty() {
-            types.push(type_def);
-        }
+        type_defs.extend(variant.type_def.clone());
     }
 
-    quote! {
-        #(#[allow(dead_code)] #types)*
+    let enum_ident = &parent.identifier;
 
-        impl #ident {
-            #(#[allow(dead_code)] #functions)*
+    quote! {
+        #(#[allow(unused)] #type_defs)*
+
+        #[automatically_derived]
+        #[allow(unused)]
+        impl #enum_ident {
+            #(#functions)*
         }
-    }.into()
+    }
+    .into()
 }
 
 #[cfg(test)]
 mod tests {
     use proc_macro2::TokenStream;
-    use syn::{DeriveInput, Macro};
     use std::str::FromStr;
+    use syn::{DeriveInput, Macro};
 
     #[test]
     fn unwrap_as_wrong_target() {
@@ -140,10 +65,12 @@ mod tests {
 
     #[test]
     fn unwrap_as() {
-        let source = "enum Test { A, B(u32), C(u32, u32), D { a: u32, b: u32 } }";
+        let source =
+            "#[derive(UnwrapAs)] pub enum Test { A, B(u32), C(i32, i64), D { a: u32, b: char }, }";
         let tokens = TokenStream::from_str(source).unwrap();
         let input: DeriveInput = syn::parse2(tokens).unwrap();
         let result = super::unwrap_as(input);
+
         println!("{result}");
     }
 }
